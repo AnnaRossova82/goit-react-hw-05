518818384426f77bf279d1d50055c5b8dfe28405
AnnaRossova <rossova.anna@gmail.com>
2024-04-28 00:16:02 +0300

    modify back

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
-import { useParams, Link, Outlet } from "react-router-dom";
+import { useState, useEffect, Suspense, useRef } from 'react';
+import { useParams, Link, Outlet, useLocation } from "react-router-dom";
 import axios from 'axios';
-import css from'./MovieDetailsPage.module.css'
+import css from './MovieDetailsPage.module.css';
 
 const apiKey = '677edfed3e4b0a50247438fda07b9881'; 
 
 const MovieDetailsPage = () => { 
   const { movieId } = useParams(); 
+  const location = useLocation();
+  const backLinkURLRef = useRef(location.state ?? "/movies");
 
   const [movieDetails, setMovieDetails] = useState(null);
   const [error, setError] = useState(null);
@@ -42,35 +44,31 @@ const MovieDetailsPage = () => {
 
   return (
     <div>
-      <Link to="/">Go back</Link>
+      <Link to={backLinkURLRef.current}>Go back</Link>
       <h2>{movieDetails.title}</h2>
       <div className={css.container}>
-      <img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} />
-      <div>
-        <h3>Overview:</h3>
-        <p>{movieDetails.overview}</p>
-        <h3>User Score:</h3>
-        <p>{movieDetails.vote_average}</p>
-        <h3>Genres:</h3>
-        <ul>
-          {movieDetails.genres.map(genre => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
-
+        <img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} />
+        <div>
+          <h3>Overview:</h3>
+          <p>{movieDetails.overview}</p>
+          <h3>User Score:</h3>
+          <p>{movieDetails.vote_average}</p>
+          <h3>Genres:</h3>
+          <ul>
+            {movieDetails.genres.map(genre => (
+              <li key={genre.id}>{genre.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-     
-      </div>
-
-        <h3>Additional Information:</h3>
-        <ul>
-
-          <li><Link to={`cast`}>Cast Preview</Link></li>
-          <li><Link to={`reviews`}>Reviews</Link></li>
-
-        </ul>
+      <h3>Additional Information:</h3>
+      <ul>
+        <li><Link to={`cast`}>Cast Preview</Link></li>
+        <li><Link to={`reviews`}>Reviews</Link></li>
+      </ul>
+      <Suspense fallback={<p>Loading...</p>}>
         <Outlet />
-     
+      </Suspense>
     </div>
   );
 };
